8e8fb698c567d61500c6f79547ebca7ee900c771
EssenceBrown <essence@cccareers.org>
2022-12-12 16:32:42 -0500

    adding to app

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+const Mortgage = require('../../src/lib/Mortagage');
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        It works!
-      </div>
-    );
+  constructor(props){
+    super(props);
+    this.state = {
+      principle:0,
+      interestRate: 0,
+      loanTerm: 0,
+      period: 12,
+      monthlyPayments: 0,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.calculateMonthlyPayment = this.calculateMonthlyPayment.bind(this)
+  }
+
+  handleChange(event){
+    switch(event.target.name){
+      case 'principle':
+          this.setState({principle:Number(event.target.value) })
+          break;
+        case 'loanTerm': this
+    }
   }
 }
